36a155651d83369b70a7dab6fbdca2e84af846b1
Johann Philipp Strathausen <strathausen@gmail.com>
2011-01-14 09:42:48 +0100

    IE 8 compatibility

--- js/dracula_graph.js
+++ js/dracula_graph.js
@@ -514,3 +514,21 @@ Raphael.el.tooltip = function (tp) {
         });
     return this;
 };
+
+/* For IE */
+if (!Array.prototype.forEach)
+{
+  Array.prototype.forEach = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+        fun.call(thisp, this[i], i, this);
+    }
+  };
+}
