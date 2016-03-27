8611ea5877dd9e7e19848188cdabe71c5ac4737c
Johann Philipp Strathausen <strathausen@gmail.com>
2016-03-27 13:40:27 +0200

    some testing

--- src/Dracula.Spring.spec.js
+++ src/Dracula.Spring.spec.js
@@ -4,10 +4,21 @@ import assert from 'assert'
 
 describe('Spring Layout', () => {
 
+  let graph
+
   it('should not crash', () => {
-    let graph = Dracula.create()
+    graph = Dracula.create()
     let edge = graph.addEdge('a', 'b')
     let layout = new Spring(graph)
+  })
+
+  it('have a distance between the nodes', () => {
+    let node1 = graph.nodes.a
+    let node2 = graph.nodes.b
+    let diffX = Math.abs(node1.layoutPosX - node2.layoutPosX)
+    let diffY = Math.abs(node1.layoutPosY - node2.layoutPosY)
+    assert(diffX > 0)
+    assert(diffY > 0)
   })
 
 })
