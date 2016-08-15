ff863885ef4dac779c0d887fb4d0d9d7605fe80a
Johann Philipp Strathausen <strathausen@gmail.com>
2016-08-15 13:51:42 +0200

    directory of submodules

---
+++ main.js
@@ -0,0 +1,24 @@
+// Core
+var dracula = require('./dist/dracula')
+
+// Layouts
+var spring = require('./dist/layout/spring')
+var orderedTree = require('./dist/layout/ordered_tree')
+var tournamentTree = require('./dist/layout/tournament_tree')
+
+// Renderers
+var raphael = require('./dist/renderer/raphael')
+var snap = require('./dist/renderer/snap')
+
+module.exports = dracula
+
+module.exports.Layout = {
+  OrderedTree: orderedTree,
+  Spring: spring,
+  TournamentTree: tournamentTree
+}
+
+module.renderer = {
+  Raphael: raphael,
+  Snap: snap
+}
