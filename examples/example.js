c08aed3bbfadabd2aa3fc996827718e35b634a41
Johann Philipp Strathausen <philipp@stratha.us>
2017-12-18 16:11:29 +0100

    make example work with less features

--- examples/example.js
+++ examples/example.js
@@ -6,47 +6,49 @@ window.onload = function() {
   var width = 600;
   var height = 400;
 
-  var g = new Graph();
+  var g = new Dracula.Graph();
 
   /* add a simple node */
   g.addNode("strawberry");
   g.addNode("cherry");
 
   /* add a node with a customized label */
-  g.addNode("1", { label : "Tomato" });
+  g.addNode("1", { label: "Tomato" });
 
 
   /* add a node with a customized shape
      (the Raphael graph drawing implementation can draw this shape, please
      consult the RaphaelJS reference for details http://raphaeljs.com/) */
   var render = function(r, n) {
-    var label = r.text(0, 30, n.label).attr({opacity:0});
-    //the Raphael set is obligatory, containing all you want to display 
+    var label = r.text(0, 30, n.label).attr({ opacity: 0 });
+    //the Raphael set is obligatory, containing all you want to display
     var set = r.set()
-      .push(r.rect(-30, -13, 62, 86)
-        .attr({ fill: '#fa8', 'stroke-width': 2, r: 9 })
+      .push(
+        r.rect(-30, -13, 62, 86)
+          .attr({ fill: '#fa8', 'stroke-width': 2, r: 9 })
       )
       .push(label);
 
-    // make the label show only on hover 
+    // make the label show only on hover
     set.hover(
       function mouseIn() {
         label.animate({ opacity: 1, 'fill-opacity': 1 }, 500);
       },
       function mouseOut() {
-        label.animate({opacity:0},300);
+        label.animate({ opacity: 0 }, 300);
       }
     );
 
-    var tooltip = r.set()
-      .push(
-        r.rect(0, 0, 90, 30).attr({ fill: '#fec', 'stroke-width': 1, r: 9 })
-      ).push(
-        r.text(25, 15, 'overlay').attr({ fill: '#000000' })
-      );
-    for(var i in set.items) {
-      set.items[i].tooltip(tooltip);
-    }
+    // TODO currently not implemented
+    // var tooltip = r.set()
+    //   .push(
+    //     r.rect(0, 0, 90, 30).attr({ fill: '#fec', 'stroke-width': 1, r: 9 })
+    //   ).push(
+    //     r.text(25, 15, 'overlay').attr({ fill: '#000000' })
+    //   );
+    // for(var i in set.items) {
+    //   set.items[i].tooltip(tooltip);
+    // }
     //            set.tooltip(r.set().push(r.rect(0, 0, 30, 30).attr({"fill": "#fec", "stroke-width": 1, r : "9px"})).hide());
     return set;
   };
@@ -57,6 +59,7 @@ window.onload = function() {
     /* arguments: r = Raphael object, n : node object */
     render: render
   });
+  // TODO not implemented yet
   //    g.addNode("Wheat", {
   /* filling the shape with a color makes it easier to be dragged */
   /* arguments: r = Raphael object, n : node object */
@@ -98,7 +101,10 @@ window.onload = function() {
   g.addEdge('1', 'cherry', { directed: true } );
 
   /* customize the colors of that edge */
-  g.addEdge('id35', 'apple', { stroke: '#bfa' , fill: '#56f', label: 'Meat-to-Apple' });
+  // TODO currently not implemented
+  g.addEdge('id35', 'apple', {
+    stroke: '#bfa' , fill: '#56f', label: 'Meat-to-Apple'
+  });
 
   /* add an unknown node implicitly by adding an edge */
   g.addEdge('strawberry', 'apple');
@@ -106,10 +112,10 @@ window.onload = function() {
   //g.removeNode("1");
 
   /* layout the graph using the Spring layout implementation */
-  var layouter = new Graph.Layout.Spring(g);
+  var layouter = new Dracula.Layout.Spring(g);
 
   /* draw the graph using the RaphaelJS draw implementation */
-  var renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
+  var renderer = new Dracula.Renderer.Raphael('#paper', g, width, height);
 
   redraw = function() {
     layouter.layout();
@@ -122,6 +128,5 @@ window.onload = function() {
     g.nodes[id].show();
   };
   //    console.log(g.nodes["kiwi"]);
-  //redraw();
+  redraw();
 };
-
