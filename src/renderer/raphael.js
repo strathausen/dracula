1d1e404edd80bd3e4fddded9b89fc6e031f22f2e
Johann Philipp Strathausen <philipp@stratha.us>
2017-12-18 16:08:54 +0100

    better var name

--- src/renderer/raphael.js
+++ src/renderer/raphael.js
@@ -187,14 +187,14 @@ Raphael.fn.connection = function Connection(obj1, obj2, style) {
         // vector normalisation to specified length
         const norm = (x, l) => -x * (l || 5) / mag
         // calculate array coordinates (two lines orthogonal to the path vector)
-        const arr = [{
+        const arc = [{
           x: (norm(x4 - x3) + norm(y4 - y3) + x4).toFixed(3),
           y: (norm(y4 - y3) + norm(x4 - x3) + y4).toFixed(3),
         }, {
           x: (norm(x4 - x3) - norm(y4 - y3) + x4).toFixed(3),
           y: (norm(y4 - y3) - norm(x4 - x3) + y4).toFixed(3),
         }]
-        path = `${path},M${arr[0].x},${arr[0].y},L${x4},${y4},L${arr[1].x},${arr[1].y}`
+        path = `${path},M${arc[0].x},${arc[0].y},L${x4},${y4},L${arc[1].x},${arc[1].y}`
       }
 
       /* function to be used for moving existent path(s), e.g. animate() or attr() */
